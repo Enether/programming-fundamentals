079e117d5dc76e22997e832c3c9bd45aaaeec217
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-21 11:14:24 +0300

    Added first three in-class problems

---
+++ 02.Data Types and Variables/Class/CenturiesToMinutes/01.Centuries To Minutes/CenturiesToMinutes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ Write program to enter an integer number of centuries and convert it to years, days, hours and minutes.
+     */
+class CenturiesToMinutes
+{
+    const double DAYS_IN_YEAR = 365.2422;
+
+    static void Main()
+    {
+        byte centuries = byte.Parse(Console.ReadLine());
+        ushort year = (ushort)(centuries * 100);
+        uint days = (uint)(year * DAYS_IN_YEAR);
+        uint hours = days * 24;
+        ulong minutes = hours * 60;
+
+        Console.WriteLine($"{centuries} centuries = {year} years = {days} days = {hours} hours = {minutes} minutes");
+    }
+}
+
---
+++ 02.Data Types and Variables/Class/CenturiesToMinutes/03.Exact Sum of Real Numbers/ExactSumRealNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ Write program to enter n numbers and calculate and print their exact sum (without rounding).
+     */
+class ExactSumRealNumbers
+{
+    static void Main()
+    {
+        int numbersCount = int.Parse(Console.ReadLine());
+        decimal sum = 0.0M;
+
+        for (int i = 0; i < numbersCount; i++)
+        {
+            sum += decimal.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(sum);
+    }
+}
+
