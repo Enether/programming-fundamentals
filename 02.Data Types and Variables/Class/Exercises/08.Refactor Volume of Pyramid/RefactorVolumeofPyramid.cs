884ed731fae1c27497f53f9ab491ef316457caa6
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-24 21:10:18 +0300

    Added problems 8.Refactor Volume Pyramid and 9.Refactor Special Numbers. Also renamed a folder

---
+++ 02.Data Types and Variables/Class/Exercises/08.Refactor Volume of Pyramid/RefactorVolumeofPyramid.cs
@@ -0,0 +1,33 @@
+﻿/*
+ Refactor this
+ Sample Code
+double dul, sh, V = 0;
+Console.Write("Length: ");
+dul = double.Parse(Console.ReadLine());
+Console.Write("Width: ");
+sh = double.Parse(Console.ReadLine());
+Console.Write("Heigth: ");
+V = double.Parse(Console.ReadLine());
+V = (dul + sh + V) / 3;
+Console.WriteLine("Pyramid Volume: {0:F2}", V);
+ */
+
+using System;
+
+class RefactorVolumeofPyramid
+{
+    static void Main()
+    {
+        Console.Write("Length: ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.Write("Width: ");
+        double width = double.Parse(Console.ReadLine());
+
+        Console.Write("Height: ");
+        double heigth = double.Parse(Console.ReadLine());
+
+        double volume = ((length * width) * heigth) / 3;
+        Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+    }
+}
---
+++ 02.Data Types and Variables/Class/Exercises/09.Refactor Special Numbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,49 @@
+﻿/*
+ Refactor:
+ You are given a working code that is a solution to Problem 5. Special Numbers. However, the variables are improperly named, declared before they are needed and some of them are used for multiple things. Without using your previous solution, modify the code so that it is easy to read and understand.
+ Sample Code
+int kolkko = int.Parse(Console.ReadLine());
+int obshto = 0; int takova = 0; bool toe = false;
+for (int ch = 1; ch <= kolkko; ch++)
+{
+    takova = ch;
+    while (ch > 0)
+    {
+        obshto += ch % 10;
+        ch = ch / 10;
+    }
+    toe = (obshto == 5) || (obshto == 7) || (obshto == 11);
+    Console.WriteLine($"{takova} -> {toe}");
+    obshto = 0;
+    ch = takova;
+}
+ */
+
+using System;
+
+class RefactorSpecialNumbers
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        int digitsSum = 0;
+        int currentNum = 0;
+        bool isSpecial = false;
+        for (int i = 1; i <= number; i++)
+        {
+            currentNum = i;
+
+            while (i > 0)
+            {
+                digitsSum += i % 10;
+                i = i / 10;
+            }
+
+            isSpecial = (digitsSum == 5) || (digitsSum == 7) || (digitsSum == 11);
+            Console.WriteLine($"{currentNum} -> {isSpecial}");
+            digitsSum = 0;
+            i = currentNum;
+        }
+    }
+}
+
