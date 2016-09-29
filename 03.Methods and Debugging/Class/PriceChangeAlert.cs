5a41d180bc084eef8e92cce766c3bf6f5c648d66
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-30 01:16:13 +0300

    Added the solution to the 10th Methods and Debugging class problem

---
+++ 03.Methods and Debugging/Class/PriceChangeAlert.cs
@@ -0,0 +1,60 @@
+﻿/*
+You are assigned to rework a given piece of code which is working without bugs but is not properly formatted. 
+The given program tracks stock prices and gives updates about the significance in each price change. Based on the significance, there are four kind of changes: no change at all (price is equal to the previous), minor (difference is below the significance threshold), price up and price down. 
+You can find the broken code here: Broken Code for Refactoring.
+*/
+
+using System;
+
+class PriceChangeAlert
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double significanceTreshold = double. Parse(Console.ReadLine());
+        double lastStock = double.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            double stock = double.Parse(Console.ReadLine());
+            double difference = GetProcentageDifference(lastStock, stock);
+            bool isSignificantDifference = IsSignificantDifference(difference, significanceTreshold);
+
+            string message = GetMessage(stock, lastStock, difference, isSignificantDifference);
+            Console.WriteLine(message);
+
+            lastStock = stock;
+        }
+    }
+
+    private static string GetMessage(double stock, double lastStock, double difference, bool isSignificantDiff)
+    {
+        string msg = "";
+        if (!isSignificantDiff)
+            msg = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)", lastStock, stock, difference);
+
+        else if (isSignificantDiff && (difference > 0))
+            msg = string.Format("PRICE UP: {0} to {1} ({2:F2}%)", lastStock, stock, difference);
+
+        else if (isSignificantDiff && (difference < 0))
+            msg = string.Format("PRICE DOWN: {0} to {1} ({2:F2}%)", lastStock, stock, difference);
+
+        else if (difference == 0)
+            msg = string.Format("NO CHANGE: {0}", stock);
+
+        return msg;
+    }
+
+    private static bool IsSignificantDifference(double significantDifference, double difference)
+    {
+        if (Math.Abs(significantDifference) >= difference)
+            return true;
+
+        return false;
+    }
+
+    private static double GetProcentageDifference(double stockOne, double stockTwo)
+    {
+        return (stockTwo - stockOne) / stockOne;
+    }
+}
