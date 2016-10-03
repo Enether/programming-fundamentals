37156234d42e23bd13edad1189cfd3798cba7c30
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-10-03 15:31:18 +0300

    Added some solutions that I missed previously

--- 03.Methods and Debugging/Class/07.GreaterOfTwoValues/GreaterOfTwoValues.cs
+++ 03.Methods and Debugging/Class/07.GreaterOfTwoValues/GreaterOfTwoValues.cs
@@ -15,15 +15,32 @@ class GreaterOfTwoValues
         switch (type)
         {
             case "int":
-                Console.WriteLine(GetMax(int.Parse(firstValue), int.Parse(secondValue));
+                Console.WriteLine(GetMax(int.Parse(firstValue), int.Parse(secondValue)));
                 break;
             case "string":
                 Console.WriteLine(GetMax(firstValue, secondValue));
                 break;
             case "char":
-                Console.WriteLine(GetMax(firstValue, secondValue));
+                Console.WriteLine(GetMax(char.Parse(firstValue), char.Parse(secondValue)));
                 break;
         }
     }
+
+    static string GetMax(string a, string b)
+    {
+        return a.CompareTo(b) >= 0 ? a : b;
+    }
+
+    static char GetMax(char a, char b)
+    {
+        return a >= b ? a : b;
+    }
+
+    static int GetMax(int a, int b)
+    {
+        return a >= b ? a : b;
+    }
+
+
 }
 
---
+++ 03.Methods and Debugging/Class/08.MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//Create a program that reads an integer number and multiplies the sum of all its even digits by the sum of all its odd digits:
+
+class MultiplyEvensByOdds
+{
+    static void Main()
+    {
+        char[] number = Console.ReadLine().ToCharArray();
+        Console.WriteLine(getDigitsSum(number));
+    }
+
+    static int getDigitsSum(char[] number)
+    {
+        int oddDigitsSum = 0;
+        int evenDigitsSum = 0;
+
+        // add up the digits
+        foreach (char digit in number)
+        {
+            if (digit != '-')  // to combat negative numbers
+            {
+                int digitInteger = int.Parse(digit.ToString());
+
+                oddDigitsSum += (digitInteger % 2 != 0) ? digitInteger : 0;
+                evenDigitsSum += (digitInteger % 2 == 0) ? digitInteger : 0;
+            }
+        }
+
+        return oddDigitsSum * evenDigitsSum;
+    }
+}
