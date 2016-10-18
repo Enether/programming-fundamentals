1fefe243540dae503ea80ff0a37fb2d310faeb6d
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-10-18 15:02:41 +0300

    Added more solutions to the homework problems for the Strings and RegEx lecture

---
+++ 08.Strings and RegEx/Homework/10.ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.ExtractEmails
+{
+    class ExtractEmails
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<user>[0-9a-zA-Z][0-9a-zA-Z.\-_]*[0-9a-zA-Z])@(?<host>[a-zA-Z][a-zA-Z\-]*[a-zA-Z])(\.[a-zA-Z][a-zA-Z\-]*[a-zA-Z])+";
+        }
+    }
+}
