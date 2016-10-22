57a7d12b2ccfca5a22aac5f3dd26bb30675781a2
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-10-22 21:26:13 +0300

    Added solutions to a second sample exam. 400/400

---
+++ Sample_Exam_II/02.SoftUniWaterSuppies/SoftUniWaterSupplies.cs
@@ -0,0 +1,145 @@
+﻿/*
+In the SoftUni office we drink lots of water (and other drinks). That’s why instead of using glasses, we use bottles.
+Everyone is using different bottles, but with the same capacity. You are given an array of floating-point numbers, in which each index represents someone’s bottle.
+We have some water in stock, but sometimes it is not enough to fill our bottles. That’s why we need you. Your job is to create a program that fills our bottles and tells us if there is enough water or not. If there is enough water to fill all bottles, you need to tell us how much water we have left. If there are some bottles that are not completely filled, you need to tell us how many are those bottles, and how much more water we need in order to fill them. We also need to know which members of the team need to wait for more water, and instead of using names, we will use the indexes of the bottles.
+On the first line you are given the total amount of water that we have. On the second line you are given all bottles that you need to fill. There is a tricky part. If the total water is an even number, you will start filling the bottles from the beginning of the array, until you run out of water, or fill all bottles. If the number is odd, you will traverse the array from the end to the beginning, filling the bottles that way. In case you run out of water, you need to print the indexes of the empty bottles in the same order in which you are traversing the array. On the last line you will receive the maximum capacity that each bottle has.
+Input
+On the first line you will receive integer, which represents the total amount of water that we have.
+On the second line you will receive an array, representing the bottles that you need to fill.
+On the last line you will receive the capacity for each bottle of the array.
+
+Output
+If there is enough water to fill all bottles print the following lines:
+“Enough water!”
+“Water left: {amountOfWaterLeft}l.”
+If there isn’t enough water print the following lines:
+“We need more water!”
+“Bottles left: {amountOfBottles}”
+“With indexes: {index}, {index}, {index}”
+The order of the indexes must be the same, as the order in which you are traversing the array.
+“We need {amountOfWaterNeeded} more liters!”
+
+Constraints
+The total amount of water will be integer in range [0…2,147,483,647].
+The items in the array will be floating-point numbers in range [0…1.7*10308].
+The bottle capacity will be integer in range [0…2,147,483,647].
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SoftUniWaterSupplies
+{
+    static void Main()
+    {
+        double totalWaterAmount = double.Parse(Console.ReadLine());
+        List<double> waterBottles = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
+        int bottleCapacity = int.Parse(Console.ReadLine());
+
+        // a tuple containing the water left and the indexes of the bottles that need filling
+        Tuple<double, List<int>> waterLeftAndBottleIndexes = null;
+        if (totalWaterAmount % 2 == 0)
+        {
+            waterLeftAndBottleIndexes = FillBottlesForwards(waterBottles, bottleCapacity, totalWaterAmount);
+        }
+        else
+        {
+            waterLeftAndBottleIndexes = FillBottlesBackwards(waterBottles, bottleCapacity, totalWaterAmount);
+        }
+
+        double waterLeft = waterLeftAndBottleIndexes.Item1;
+
+        if (waterLeft >= 0)
+        {
+            Console.WriteLine("Enough water!");
+            Console.WriteLine($"Water left: {waterLeft}l.");
+        }
+        else
+        {
+            var leftBottlesIndexes = waterLeftAndBottleIndexes.Item2;
+            Console.WriteLine("We need more water!");
+            Console.WriteLine($"Bottles left: {leftBottlesIndexes.Count}");
+            Console.WriteLine($"With indexes: {string.Join(", ", leftBottlesIndexes)}");
+            Console.WriteLine($"We need {((long)bottleCapacity * waterBottles.Count) - waterBottles.Sum()} more liters!");
+        }
+    }
+
+    static Tuple<double, List<int>> FillBottlesForwards(List<double> waterBottles, int bottleCapacity, double totalWaterAmount)
+    {
+        for (int i = 0; i < waterBottles.Count; i++)
+        {
+            if (waterBottles[i] < bottleCapacity)
+            {
+                double waterNeeded = bottleCapacity - waterBottles[i];
+                if (totalWaterAmount - waterNeeded >= 0)
+                {
+                    // fill the bottle
+                    totalWaterAmount -= waterNeeded;
+                    waterBottles[i] = bottleCapacity;
+                }
+                else
+                {
+                    // we don't have enough water left
+                    waterBottles[i] += totalWaterAmount;
+                    totalWaterAmount = -1;
+                    break;
+                }
+            }
+        }
+
+        if (totalWaterAmount >= 0)
+            return new Tuple<double, List<int>>(totalWaterAmount, new List<int>(){ });
+        else
+        {
+            // water was not enough, get the indexes that are not full
+            List<int> bottleIndexesThatNeedFilling = new List<int>();
+            for (int i = 0; i < waterBottles.Count; i++)
+            {
+                if (waterBottles[i] < bottleCapacity)
+                    bottleIndexesThatNeedFilling.Add(i);
+            }
+
+            return new Tuple<double, List<int>>(totalWaterAmount, bottleIndexesThatNeedFilling);
+        }
+    }
+
+    static Tuple<double, List<int>> FillBottlesBackwards(List<double> waterBottles, int bottleCapacity, double totalWaterAmount)
+    {
+        for (int i = waterBottles.Count-1; i >= 0; i--)
+        {
+            if (waterBottles[i] < bottleCapacity)
+            {
+                double waterNeeded = bottleCapacity - waterBottles[i];
+                if (totalWaterAmount - waterNeeded >= 0)
+                {
+                    // fill the bottle
+                    totalWaterAmount -= waterNeeded;
+                    waterBottles[i] = bottleCapacity;
+                }
+                else
+                {
+                    // we don't have enough water left
+                    waterBottles[i] += totalWaterAmount;
+                    totalWaterAmount = -1;
+                    break;
+                }
+            }
+        }
+
+        if (totalWaterAmount >= 0)
+            return new Tuple<double, List<int>>(totalWaterAmount, new List<int>() { });
+        else
+        {
+            // water was not enough, get the indexes that are not full
+            List<int> bottleIndexesThatNeedFilling = new List<int>();
+            for (int i = waterBottles.Count - 1; i >= 0; i--)
+            {
+                if (waterBottles[i] < bottleCapacity)
+                    bottleIndexesThatNeedFilling.Add(i);
+            }
+
+            return new Tuple<double, List<int>>(totalWaterAmount, bottleIndexesThatNeedFilling);
+        }
+    }
+}
+
---
+++ Sample_Exam_II/03.MatrixOperator/MatrixOperator.cs
@@ -0,0 +1,148 @@
+﻿/*
+You are matrix operator chief, but you are tired of doing your job manually. You want to write a program, that will automate the process. The information you are processing comes from your boss, and he wants to see daily reports for the job.
+You are given a table of integers. For that table you will need to execute some commands.
+The commands are the following: remove, swap and insert. All commands come with 2 additional parameters. 
+The remove command – type and position.
+The type can be one of the following – positive / negative / odd / even. That means that you will need to remove said elements from the given row / col.
+The position will be in the following format: {row / col index}.
+The swap command – the 2 rows that you need to swap.
+The insert command – row and number that you need to insert at the beginning of the given row.
+The input stops once you receive the “end” command, and then you need to print the table after all operations.
+
+Input
+On the first line you will receive integer r – rows.
+On the next r lines, you will receive the elements for each row.
+On the next lines, you will receive commands in the following format:
+remove {type} {position}
+swap {firstRow} {secondRow}
+insert {row} {element}
+The input stops when you receive the command “end”.
+
+Output
+The output should consist of the matrix after all commands have been executed.
+
+Constraints
+The rows of the table will be in range [1…30].
+The columns of each row will be in range [0…30].
+The elements of the table will be integers in the range [-2,147,483,648…2,147,483,647].
+The commands will always be valid and in the given format.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MatrixOperator
+{
+    static void Main(string[] args)
+    {
+        int matrixRows = int.Parse(Console.ReadLine());
+        int[][] matrix = new int[matrixRows][];
+        for (int i = 0; i < matrixRows; i++)
+        {
+            int[] row = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            matrix[i] = row;
+        }
+
+        // handle commands
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "end")
+                break;
+
+            string[] commandInfo = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            switch(commandInfo[0])
+            {
+                case "remove":
+                    string removeType = commandInfo[1];
+                    string rowOrCol = commandInfo[2]; // string indicating where to remove ex: "col" will remove every element from the given column
+                    int rowOrColIndex = int.Parse(commandInfo[3]);
+                    HandleRemoveCommand(removeType, rowOrCol, rowOrColIndex, matrix);
+                    break;
+                case "swap":
+                    // swap two rows
+                    int firstRowIdx = int.Parse(commandInfo[1]);
+                    int secondRowIdx = int.Parse(commandInfo[2]);
+
+                    // swap
+                    int[] firstRow = matrix[firstRowIdx];
+                    matrix[firstRowIdx] = matrix[secondRowIdx];
+                    matrix[secondRowIdx] = firstRow;
+                    break;
+                case "insert":
+                    // insert number at the beggining of row
+                    int rowIndex = int.Parse(commandInfo[1]);
+                    int elementToInsert = int.Parse(commandInfo[2]);
+                    List<int> tempList = matrix[rowIndex].ToList();
+                    tempList.Insert(0, elementToInsert);
+                    matrix[rowIndex] = tempList.ToArray();
+                    break;
+            }
+        }
+        PrintMatrix(matrix);
+    }
+
+    static void HandleRemoveCommand(string removeType, string rowOrCol, int index, int[][] matrix)
+    {
+        if (rowOrCol == "row")
+        {
+            // remove from row
+            int[] updatedRow = null;
+            switch (removeType)
+            {
+                case "even":
+                    // get only odd elements
+                    updatedRow = matrix[index].Where(x => x % 2 != 0).Select(x => x).ToArray();
+                    break;
+                case "odd":
+                    // get only even elements
+                    updatedRow = matrix[index].Where(x => x % 2 == 0).Select(x => x).ToArray();
+                    break;
+                case "positive":
+                    // get only negative elements
+                    updatedRow = matrix[index].Where(x => x < 0).Select(x => x).ToArray();
+                    break;
+                case "negative":
+                    // get only positive elements
+                    updatedRow = matrix[index].Where(x => x >= 0).Select(x => x).ToArray();
+                    break;
+            }
+
+            matrix[index] = updatedRow;
+        }
+        else
+        {
+            // through cols
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                if (matrix[row].Length > index)  // if there is a col at the desired position in the current row
+                {
+                    List<int> tempList = matrix[row].ToList();
+
+                    if (removeType == "even" && tempList[index] % 2 == 0)
+                        tempList.RemoveAt(index);
+                    else if (removeType == "odd" && tempList[index] % 2 != 0)
+                        tempList.RemoveAt(index);
+                    else if (removeType == "positive" && tempList[index] >= 0)
+                        tempList.RemoveAt(index);
+                    else if (removeType == "negative" && tempList[index] < 0)
+                        tempList.RemoveAt(index);
+
+                    matrix[row] = tempList.ToArray();
+                }
+            }
+        }
+
+    }
+
+    static void PrintMatrix(int[][] matrix)
+    {
+        foreach (var row in matrix)
+        {
+            Console.WriteLine(string.Join(" ", row));
+        }
+    }
+}
+
